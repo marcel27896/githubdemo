63e5f16be6e6c22aeede0b4c8732a354490f2805
marcel27896 <adelzarajhonmarcel@gmail.com>
2024-06-24 10:43:14 +0800

    adding a regex to evaluate a condition

--- githubpractice.java
+++ githubpractice.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class githubpractice {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name ");
-            String name = scanner.nexliLine();
+
+        System.out.println("Enter your name : ");
+        String name = scanner.nextLine();
+
+        Pattern p = Pattern.compile("[a-zA-Z]+");
+        Matcher m = p.matcher(name);
+
+        if (m.matches()) {
+            System.out.println("name valid");
+        }else{
+            System.out.println("name not");
+        }
+        scanner.close();
     }
 }
