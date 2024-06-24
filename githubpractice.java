6f3fd1a4a0f860b418fda8b402e19ba5d4af771f
marcel27896 <adelzarajhonmarcel@gmail.com>
2024-06-24 09:59:04 +0800

    this is for the third brance

--- githubpractice.java
+++ githubpractice.java
@@ -1,6 +1,6 @@
 public class githubpractice {
 
     public static void main(String[] args) {
-        System.out.println("this test is for another brance name new");
+        System.out.println("this is for the third branch");
     }
 }
