6bec0bc47caa6f2d8cf291836d165931d39ab269
marcel27896 <adelzarajhonmarcel@gmail.com>
2024-06-24 10:08:54 +0800

    made a fuckin changes

--- githubpractice.java
+++ githubpractice.java
@@ -1,6 +1,6 @@
 public class githubpractice {
 
     public static void main(String[] args) {
-        System.out.println("this is for the third branch");
+        System.out.println("this is the fuckin main branch");
     }
 }
